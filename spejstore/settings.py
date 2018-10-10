95694890ea29ab2b2f1d27281dc31787bf1ec345
d42 <d42@users.noreply.github.com>
2018-10-10 21:33:16 +0200

    associate_by_email

--- spejstore/settings.py
+++ spejstore/settings.py
@@ -134,12 +134,13 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
     'auth.pipeline.staff_me_up',
-    'social_core.pipeline.social_auth.associate_by_email',)
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
