0fa9762bea6bd66bcc88dd67a5110804ed95d3ae
Dariusz Niemczyk <palid@hackerspace.pl>
2023-08-25 21:36:19 +0200

    auth: do not automatically staff new members

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -2,15 +2,6 @@ from social_core.pipeline.social_auth import associate_by_email
 from django.contrib.auth.models import Group
 
 
-def staff_me_up(backend, details, response, uid, user, *args, **kwargs):
-    user.is_staff = True
-    try:
-        user.groups.set([Group.objects.get(name="member")])
-    except Group.DoesNotExist:
-        pass
-    user.save()
-
-
 def associate_by_personal_email(backend, details, user=None, *args, **kwargs):
     return associate_by_email(
         backend,
